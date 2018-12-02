fcc5762f7246f6510da3a5c5f4810b036940fcf4
dzllikelsw@163.com <dongzl>
2018-12-02 22:27:27 +0800

    lock support test class
    lock support test class

---
+++ src/main/java/com/thread/lock/locksupport/LockSupportTest.java
@@ -0,0 +1,35 @@
+package com.thread.lock.locksupport;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2018/12/02 下午9:52
+ */
+public class LockSupportTest {
+
+    private static Thread mainThread;
+
+    public static void main(String[] args) throws Exception {
+        ThreadA ta = new ThreadA("ta");
+        mainThread = Thread.currentThread();
+        System.out.println(Thread.currentThread().getName() + " start ta");
+        ta.start();
+        System.out.println(Thread.currentThread().getName() + " block");
+        LockSupport.park(mainThread);
+        System.out.println(Thread.currentThread().getName() + " continue");
+    }
+
+    static class ThreadA extends Thread {
+
+        public ThreadA(String name) {
+            super(name);
+        }
+
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + " wakeup others");
+            LockSupport.unpark(mainThread);
+        }
+    }
+}
