7759ac0bb7b14d1048599830a34274dd46802959
dongzl <dzllikelsw@163.com>
2018-12-10 17:21:58 +0800

    Synchronized code block test

---
+++ Java-Multi-Thread-Programming/src/main/java/com/multi/thread/chapter2/section2/example3/Task.java
@@ -0,0 +1,31 @@
+package com.multi.thread.chapter2.section2.example3;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2018/12/10 下午4:54
+ */
+public class Task {
+
+    private String getData1;
+
+    private String getData2;
+
+    public synchronized void doLongTimeTask() {
+        try {
+            System.out.println("begin task");
+            Thread.sleep(3000);
+            String privateGetData1 = "长时间处理任务后从远程返回的值1 threadName = " + Thread.currentThread().getName();
+            String privateGetData2 = "长时间处理任务后从远程返回的值2 threadName = " + Thread.currentThread().getName();
+            synchronized (this) {
+                getData1 = privateGetData1;
+                getData2 = privateGetData2;
+            }
+            System.out.println(getData1);
+            System.out.println(getData2);
+            System.out.println("end Task!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
