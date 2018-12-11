0aa81be26eb5afd2b3c4f63422f6002effd4517f
dongzl <dzllikelsw@163.com>
2018-12-11 13:52:53 +0800

    Chapter2 section2 2.2.8 test code

--- Java-Multi-Thread-Programming/src/main/java/com/multi/thread/chapter2/section2/example12/MyObject.java
+++ Java-Multi-Thread-Programming/src/main/java/com/multi/thread/chapter2/section2/example12/MyObject.java
@@ -7,9 +7,17 @@ package com.multi.thread.chapter2.section2.example12;
  */
 public class MyObject {
 
-    public synchronized void speedPrintString() {
+    /*public synchronized void speedPrintString() {
         System.out.println("speedPringString______getLock time = " + System.currentTimeMillis() + "run ThreadName = " + Thread.currentThread().getName());
         System.out.println("------------------------");
         System.out.println("speedPrintString releaseLock time = " + System.currentTimeMillis() + "run ThreadName = " + Thread.currentThread().getName());
+    }*/
+
+    public void speedPrintString() {
+        synchronized (this) {
+            System.out.println("speedPringString______getLock time = " + System.currentTimeMillis() + "run ThreadName = " + Thread.currentThread().getName());
+            System.out.println("------------------------");
+            System.out.println("speedPrintString releaseLock time = " + System.currentTimeMillis() + "run ThreadName = " + Thread.currentThread().getName());
+        }
     }
 }
