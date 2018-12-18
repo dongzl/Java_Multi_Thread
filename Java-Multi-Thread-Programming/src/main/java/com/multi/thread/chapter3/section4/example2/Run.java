5e84e86a934a37ae5c49ec33ce82e7643d34eb51
dongzl <dzllikelsw@163.com>
2018-12-18 09:42:05 +0800

    Chapter3 section1 3.4.2 test code

---
+++ Java-Multi-Thread-Programming/src/main/java/com/multi/thread/chapter3/section4/example2/Run.java
@@ -0,0 +1,23 @@
+package com.multi.thread.chapter3.section4.example2;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2018/12/18 上午9:34
+ */
+public class Run {
+
+    public static void main(String[] args) throws Exception {
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("在Main线程中取值 = " + Tools.tl.get());
+                Thread.sleep(100);
+            }
+            Thread.sleep(5000);
+            ThreadA a = new ThreadA();
+            a.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
