b1c792b9162fcf81f3d2e4137cb840a95f9cce2e
dongzl <dzllikelsw@163.com>
2018-12-22 13:35:16 +0800

    Chapter5 section1 5.1.4 test code

---
+++ Java-Multi-Thread-Programming/src/main/java/com/multi/thread/chapter5/section1/example4/Run.java
@@ -0,0 +1,35 @@
+package com.multi.thread.chapter5.section1.example4;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2018/12/22 下午1:03
+ */
+public class Run {
+
+    public static class MyTask extends TimerTask {
+        @Override
+        public void run() {
+            try {
+                System.out.println("运行了！时间为：" + new Date());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            MyTask task = new MyTask();
+            Timer timer = new Timer();
+            System.out.println("当前时间：" + new Date().toLocaleString());
+            timer.schedule(task, 3000, 5000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
