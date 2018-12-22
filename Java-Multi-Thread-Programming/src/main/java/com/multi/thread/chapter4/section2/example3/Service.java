0a9203ebd500760bd50ea6866be7f052deba0d8e
dongzl <dzllikelsw@163.com>
2018-12-22 12:25:45 +0800

    Chapter4 section1 4.2.3 test code

---
+++ Java-Multi-Thread-Programming/src/main/java/com/multi/thread/chapter4/section2/example3/Service.java
@@ -0,0 +1,41 @@
+package com.multi.thread.chapter4.section2.example3;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2018/12/21 下午8:15
+ */
+public class Service {
+
+    private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public void read() {
+        try {
+            try {
+                lock.readLock().lock();
+                System.out.println("获取读锁" + Thread.currentThread().getName() + " " + System.currentTimeMillis());
+                Thread.sleep(10000);
+            } finally {
+                lock.readLock().unlock();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void write() {
+        try {
+            try {
+                lock.writeLock().lock();
+                System.out.println("获得写锁" + Thread.currentThread().getName() + " " + System.currentTimeMillis());
+                Thread.sleep(10000);
+            } finally {
+                lock.writeLock().unlock();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
