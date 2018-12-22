0bc5da5b6a71babd68fc6d51025b883c1c8c1714
dongzl <dzllikelsw@163.com>
2018-12-22 12:23:09 +0800

    Chapter4 section1 4.2.2 test code

---
+++ Java-Multi-Thread-Programming/src/main/java/com/multi/thread/chapter4/section2/example2/ThreadA.java
@@ -0,0 +1,20 @@
+package com.multi.thread.chapter4.section2.example2;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2018/12/22 下午12:16
+ */
+public class ThreadA extends Thread {
+
+    private Service service;
+
+    public ThreadA(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        service.write();
+    }
+}
