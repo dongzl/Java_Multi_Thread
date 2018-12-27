e31d960cfd4bbcc58e3e7d5107b9b4c02d8fe0a8
dongzl <dzllikelsw@163.com>
2018-12-27 18:06:48 +0800

    3.2.5 test code

--- Java-High-Concurrency-Programming/src/main/java/com/high/concurrency/chapter3/section2/RejectThreadPoolDemo.java
+++ Java-High-Concurrency-Programming/src/main/java/com/high/concurrency/chapter3/section2/RejectThreadPoolDemo.java
@@ -4,6 +4,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -27,17 +29,33 @@ public class RejectThreadPoolDemo {
     }
 
     public static void main(String[] args) throws Exception {
+//        MyTask task = new MyTask();
+//        ExecutorService exec = new ThreadPoolExecutor(5, 5, 0L,
+//                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(10),
+//                Executors.defaultThreadFactory(), new RejectedExecutionHandler() {
+//            public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+//                System.out.println(r.toString() + " is discard");
+//            }
+//        });
+//        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+//            exec.submit(task);
+//            Thread.sleep(10);
+//        }
+
         MyTask task = new MyTask();
-        ExecutorService exec = new ThreadPoolExecutor(5, 5, 0L,
-                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(10),
-                Executors.defaultThreadFactory(), new RejectedExecutionHandler() {
-            public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
-                System.out.println(r.toString() + " is discard");
+        ExecutorService es = new ThreadPoolExecutor(5, 5, 0L,
+                TimeUnit.MILLISECONDS, new SynchronousQueue<Runnable>(), new ThreadFactory() {
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                System.out.println("create " + t);
+                return t;
             }
         });
-        for (int i = 0; i < Integer.MAX_VALUE; i++) {
-            exec.submit(task);
-            Thread.sleep(10);
+
+        for (int i = 0; i < 5; i++) {
+            es.submit(task);
         }
+        Thread.sleep(2000);
     }
 }
