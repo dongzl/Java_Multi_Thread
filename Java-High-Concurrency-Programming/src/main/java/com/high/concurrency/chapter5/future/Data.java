97c28b2a1999afba5ceba97a6569119d27c239f8
dongzl <dzllikelsw@163.com>
2019-01-02 20:07:58 +0800

    Chapter5 section5 5.4.4

---
+++ Java-High-Concurrency-Programming/src/main/java/com/high/concurrency/chapter5/future/Data.java
@@ -0,0 +1,12 @@
+package com.high.concurrency.chapter5.future;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2019/01/02 下午8:06
+ */
+public interface Data {
+
+    public String getResult();
+
+}
