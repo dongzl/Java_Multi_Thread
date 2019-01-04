66a591c0ae6b2aced7a595ad7250ea78e1de1be6
dongzl <dzllikelsw@163.com>
2019-01-04 20:14:32 +0800

    Chapter6 section3 6.3

---
+++ Java-High-Concurrency-Programming/src/main/java/com/high/concurrency/chapter6/section2/Test1.java
@@ -0,0 +1,39 @@
+package com.high.concurrency.chapter6.section2;
+
+import java.util.Arrays;
+import java.util.function.IntConsumer;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2019/01/04 下午8:07
+ */
+public class Test1 {
+
+    static int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    public static void main(String[] args) throws Exception {
+        for (int i : arr) {
+            System.out.println(i);
+        }
+
+        Arrays.stream(arr).forEach(new IntConsumer() {
+            @Override
+            public void accept(int value) {
+                System.out.println(value);
+            }
+        });
+
+        Arrays.stream(arr).forEach((final int x) -> {
+            System.out.println(x);
+        });
+
+        Arrays.stream(arr).forEach((x) -> {
+            System.out.println(x);
+        });
+
+        Arrays.stream(arr).forEach((x) -> System.out.println(x));
+
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+}
