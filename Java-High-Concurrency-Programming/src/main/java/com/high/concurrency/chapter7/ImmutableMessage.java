9a570e7e8407597ef18597a200ebb2478cf17bd7
dongzl <dzllikelsw@163.com>
2019-01-07 09:48:34 +0800

    Chapter7 section3 7.3

---
+++ Java-High-Concurrency-Programming/src/main/java/com/high/concurrency/chapter7/ImmutableMessage.java
@@ -0,0 +1,30 @@
+package com.high.concurrency.chapter7;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2019/01/07 上午9:45
+ */
+public final class ImmutableMessage {
+
+    private final int sequenceNumber;
+
+    private final List<String> values;
+
+    public ImmutableMessage(int sequenceNumber, List<String> values) {
+        this.sequenceNumber = sequenceNumber;
+        this.values = Collections.unmodifiableList(new ArrayList<>(values));
+    }
+
+    public int getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    public List<String> getValues() {
+        return values;
+    }
+}
