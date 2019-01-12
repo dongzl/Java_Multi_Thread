8bb330689262018239b533b3196fe1cdc062dea1
dongzl <dzllikelsw@163.com>
2019-01-12 18:56:48 +0800

    Chapter2 section4

---
+++ Java-Multi-Thread-Guide/Java-Multi-Thread-Guide-Core/src/main/java/com/multi/thread/guide/core/chapter2/section3/AtomicityExample.java
@@ -0,0 +1,48 @@
+package com.multi.thread.guide.core.chapter2.section3;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2019/01/12 下午6:47
+ */
+public class AtomicityExample {
+
+    private HostInfo hostInfo;
+
+    public void updateHostInfo(String ip, int port) {
+        hostInfo.setIp(ip);
+        hostInfo.setPort(port);
+    }
+
+    public void connectToHost() {
+        String ip = hostInfo.getIp();
+        int port = hostInfo.getPort();
+        connectToHost(ip, port);
+    }
+
+    private void connectToHost(String ip, int port) {
+
+    }
+
+    public static class HostInfo {
+        private String ip;
+
+        private int port;
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public void setPort(int port) {
+            this.port = port;
+        }
+    }
+}
