fe1c08bb70a3b965ec78f7609145fa21a8fa11f7
dongzl <dzllikelsw@163.com>
2019-01-13 21:22:19 +0800

    LongAdder 类LongAdder 类

--- Java-Multi-Thread-Self/src/main/java/com/thread/LongAdderDemo1.java
+++ Java-Multi-Thread-Self/src/main/java/com/thread/LongAdderDemo1.java
@@ -1,8 +1,5 @@
 package com.thread;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.LongAdder;
 
 /**
@@ -12,15 +9,9 @@ import java.util.concurrent.atomic.LongAdder;
  */
 public class LongAdderDemo1 {
 
-    private static final int MAX_THREADS = 3;
-
-    private static final int TASK_COUNT = 3;
-
     private static final int TARGET_COUNT = 10000000;
 
     private LongAdder lacount = new LongAdder();
-
-    private static CountDownLatch cdladdr = new CountDownLatch(TASK_COUNT);
 
     public class LongAdderThread implements Runnable {
 
@@ -39,24 +30,6 @@ public class LongAdderDemo1 {
             }
             long endtime = System.currentTimeMillis();
             System.out.println("LongAdderThread spend:" + (endtime - starttime) + "ms" + " v" + v);
-            cdladdr.countDown();
         }
-
-    }
-
-    public void testLongAdder() throws InterruptedException {
-        ExecutorService exe = Executors.newFixedThreadPool(MAX_THREADS);
-        long starttime = System.currentTimeMillis();
-        LongAdderThread atomic = new LongAdderThread(starttime);
-        for (int i = 0; i < TASK_COUNT; i++) {
-            exe.submit(atomic);
-        }
-        cdladdr.await();
-        exe.shutdown();
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        LongAdderDemo1 demo = new LongAdderDemo1();
-        demo.testLongAdder();
     }
 }
