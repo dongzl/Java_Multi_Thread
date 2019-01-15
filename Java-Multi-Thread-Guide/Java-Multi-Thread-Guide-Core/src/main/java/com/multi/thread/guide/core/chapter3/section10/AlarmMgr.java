b1be7e19a7519e126517725af1dbee5476250421
dongzl <dzllikelsw@163.com>
2019-01-15 14:05:16 +0800

    Chapter3 section3.10.2

---
+++ Java-Multi-Thread-Guide/Java-Multi-Thread-Guide-Core/src/main/java/com/multi/thread/guide/core/chapter3/section10/AlarmMgr.java
@@ -0,0 +1,36 @@
+package com.multi.thread.guide.core.chapter3.section10;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2019/01/15 下午1:59
+ */
+public enum AlarmMgr implements Runnable {
+
+    INSTANCE;
+
+    private final AtomicBoolean initializating = new AtomicBoolean(false);
+
+    AlarmMgr() {
+
+    }
+
+    public void init() {
+        if (initializating.compareAndSet(false, true)) {
+            new Thread(this).start();
+        }
+    }
+
+    public int sendAlarm(String message) {
+        int result = 0;
+        return result;
+    }
+
+
+    @Override
+    public void run() {
+        
+    }
+}
