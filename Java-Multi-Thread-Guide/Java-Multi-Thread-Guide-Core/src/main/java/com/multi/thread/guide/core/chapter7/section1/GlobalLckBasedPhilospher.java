7a2a43a9b9f20520bc7e191648c8fd3790867345
dongzl <dzllikelsw@163.com>
2019-01-25 17:07:45 +0800

    Chapter7 section1

---
+++ Java-Multi-Thread-Guide/Java-Multi-Thread-Guide-Core/src/main/java/com/multi/thread/guide/core/chapter7/section1/GlobalLckBasedPhilospher.java
@@ -0,0 +1,28 @@
+package com.multi.thread.guide.core.chapter7.section1;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2019/01/25 下午4:59
+ */
+public class GlobalLckBasedPhilospher extends AbstractPhilosopher {
+
+    private static final Object GLOBAL_LOCK = new Object();
+
+    public GlobalLckBasedPhilospher(int id, Chopstick left, Chopstick right) {
+        super(id, left, right);
+    }
+
+    @Override
+    public void eat() {
+        synchronized (GLOBAL_LOCK) {
+            System.out.println();
+            left.pickUp();
+            System.out.println();
+            right.pickUp();
+            doEat();
+            right.putDown();
+            left.putDown();
+        }
+    }
+}
