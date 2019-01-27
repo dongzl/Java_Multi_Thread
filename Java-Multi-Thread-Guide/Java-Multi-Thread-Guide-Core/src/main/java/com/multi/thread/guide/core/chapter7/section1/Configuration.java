71d1435b1bfd8a1e01c4fd21a3ae7a5ff5358c99
dongzl <dzllikelsw@163.com>
2019-01-27 16:42:37 +0800

    Chapter7 section1

---
+++ Java-Multi-Thread-Guide/Java-Multi-Thread-Guide-Core/src/main/java/com/multi/thread/guide/core/chapter7/section1/Configuration.java
@@ -0,0 +1,48 @@
+package com.multi.thread.guide.core.chapter7.section1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2019/01/27 下午4:32
+ */
+public class Configuration {
+
+    private final String name;
+
+    private volatile int version;
+
+    private volatile Map<String, String> configItemMap;
+
+    public Configuration(String name, int version) {
+        this.name = name;
+        this.version = version;
+        configItemMap = new HashMap<>();
+    }
+
+    public void setProperty(String key, String value) {
+        configItemMap.put(key, value);
+    }
+
+    public String getProperty(String key) {
+        return configItemMap.get(key);
+    }
+
+    public void update(Map<String, String> properties) {
+        configItemMap = properties;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+}
---
+++ Java-Multi-Thread-Guide/Java-Multi-Thread-Guide-Core/src/main/java/com/multi/thread/guide/core/chapter7/section1/ConfigurationManager.java
@@ -0,0 +1,13 @@
+package com.multi.thread.guide.core.chapter7.section1;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2019/01/27 下午4:31
+ */
+public enum ConfigurationManager {
+
+    INSTANCE;
+
+    //protected final Set<>
+}
