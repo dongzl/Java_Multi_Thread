1ed535556fe673fcdecc488e3518a21a1cf1d381
dongzl <dzllikelsw@163.com>
2019-03-15 10:14:32 +0800

    Chapter2 section11

---
+++ Java-Concurrent-Programming-Beauty/src/main/java/com/beauty/thread/chapter2/TestForContent.java
@@ -0,0 +1,26 @@
+package com.beauty.thread.chapter2;
+
+/**
+ * @author dongzonglei
+ * @description
+ * @date 2019-03-15 09:59
+ */
+public class TestForContent {
+
+    static final int LINE_NUM =1024;
+
+    static final int COLUM_NUM = 1024;
+
+    public static void main(String args[]) throws Exception {
+        long[][] array = new long[LINE_NUM][COLUM_NUM];
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < LINE_NUM; i++) {
+            for (int j = 0; j < COLUM_NUM; j++) {
+                array[i][j] = i * 2 + j;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        long cacheTime = endTime - startTime;
+        System.out.println("cache time:" + cacheTime);
+    }
+}
