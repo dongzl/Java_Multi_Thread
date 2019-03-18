4787e365dede49b99fb698491899ef4b648514b7
dongzl <dzllikelsw@163.com>
2019-03-18 11:21:12 +0800

    Chapter6 section1

---
+++ Java-Concurrent-Programming-Beauty/src/main/java/com/beauty/thread/chapter6/FIFOMutex.java
@@ -0,0 +1,39 @@
+package com.beauty.thread.chapter6;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @author dongzonglei
+ * @description
+ * @date 2019-03-18 11:12
+ */
+public class FIFOMutex {
+
+    private final AtomicBoolean locked = new AtomicBoolean(false);
+    private final Queue<Thread> waiters = new ConcurrentLinkedQueue<>();
+
+    public void lock() {
+        boolean wasInterrupted = false;
+        Thread current = Thread.currentThread();
+        waiters.add(current);
+        while(waiters.peek() != current || !locked.compareAndSet(false, true)) {
+            LockSupport.park(this);
+            if (Thread.interrupted()) {
+                wasInterrupted = true;
+            }
+        }
+
+        waiters.remove();
+        if (wasInterrupted) {
+            current.interrupt();
+        }
+    }
+
+    public void unlock() {
+        locked.set(false);
+        LockSupport.unpark(waiters.peek());
+    }
+}
