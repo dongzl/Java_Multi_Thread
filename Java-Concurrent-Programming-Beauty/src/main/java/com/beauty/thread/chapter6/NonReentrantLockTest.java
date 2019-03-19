e530275b7ea70d3a9cfa049af7d0353974257d41
dongzl <dzllikelsw@163.com>
2019-03-19 10:21:46 +0800

    Chapter6 section2

---
+++ Java-Concurrent-Programming-Beauty/src/main/java/com/beauty/thread/chapter6/NonReentrantLockTest.java
@@ -0,0 +1,61 @@
+package com.beauty.thread.chapter6;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.Condition;
+
+/**
+ * @author dongzonglei
+ * @description
+ * @date 2019-03-19 10:12
+ */
+public class NonReentrantLockTest {
+
+    final static NonReentrantLock lock = new NonReentrantLock();
+    final static Condition notFull = lock.newCondition();
+    final static Condition notEmpty = lock.newCondition();
+
+    final static Queue<String> queue = new LinkedBlockingQueue<>();
+    final static int queueSize = 10;
+
+    public static void main(String args[]) throws Exception {
+        Thread producer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lock.lock();
+                try {
+                    while (queue.size() == queueSize) {
+                        notEmpty.await();
+                    }
+                    queue.add("ele");
+                    notFull.signalAll();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        });
+
+        Thread consumer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lock.lock();
+                try {
+                    while (queue.size() == 0) {
+                        notFull.await();
+                    }
+                    String ele = queue.poll();
+                    notEmpty.signalAll();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        });
+
+        producer.start();
+        consumer.start();
+    }
+}
