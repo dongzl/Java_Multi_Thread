081d8648eeb76a9a3062606d30e8ba029feb24eb
MaxQiu <yueyiqiu178@gmail.com>
2023-11-30 11:37:36 +0800

    修正chapter2.section2.example3範例程式

---
+++ Java-Multi-Thread-Programming/src/main/java/com/multi/thread/chapter2/section2/example3/MyThread1.java
@@ -0,0 +1,24 @@
+package com.multi.thread.chapter2.section2.example3;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2018/12/10 下午4:59
+ */
+public class MyThread1 extends Thread {
+
+    private Task task;
+
+    public MyThread1(Task task) {
+        super();
+        this.task = task;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        CommonUtils.beginTime1 = System.currentTimeMillis();
+        task.doLongTimeTask();
+        CommonUtils.endTime1 = System.currentTimeMillis();
+    }
+}
